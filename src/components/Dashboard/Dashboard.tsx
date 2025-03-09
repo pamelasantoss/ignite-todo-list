a821417528009acf0d2d39c9d83f813599269f7c
Pamela Santos <pamesantoss@hotmail.com>
2025-03-09 17:23:10 -0300

    fix: file formatter & validation to not allowed create an empty task

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,33 +1,50 @@
-import { ChangeEvent, useEffect, useState } from 'react'
-import { PlusCircle } from '@phosphor-icons/react'
-import { useTask } from '../../hooks/useTasks'
-import { Task } from '../Task/Task'
-import emptyClipboard from '../../assets/empty-clipboard.svg'
-import styles from './Dashboard.module.scss'
+import { ChangeEvent, useEffect, useState } from "react";
+import { PlusCircle } from "@phosphor-icons/react";
+import { useTask } from "../../hooks/useTasks";
+import { Task } from "../Task/Task";
+import emptyClipboard from "../../assets/empty-clipboard.svg";
+import styles from "./Dashboard.module.scss";
 
 export function Dashboard() {
   const [newTask, setNewTask] = useState("");
-  const { tasks, tasksDone, clearField, onCreateNewTask, onDeleteTask, onMarkTask } = useTask(newTask);
+  const {
+    tasks,
+    tasksDone,
+    clearField,
+    onCreateNewTask,
+    onDeleteTask,
+    onMarkTask,
+  } = useTask(newTask);
 
   const allTasksCreated = tasks.length;
   const allTasksFinished = tasksDone.length;
-  const tasksProgress = allTasksCreated > 0 ? `${allTasksFinished} de ${allTasksCreated}` : allTasksCreated;
+  const tasksProgress =
+    allTasksCreated > 0
+      ? `${allTasksFinished} de ${allTasksCreated}`
+      : allTasksCreated;
 
   const handleNewTaskOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewTask(event.target.value)
-  }
+    setNewTask(event.target.value);
+  };
 
   useEffect(() => {
     if (clearField) {
-      setNewTask("")
+      setNewTask("");
     }
-  }, [clearField])
+  }, [clearField]);
 
   return (
     <>
       <form onSubmit={onCreateNewTask}>
         <div className={styles.createTask}>
-          <input type="text" placeholder="Adicione uma nova tarefa" name="addTask" value={newTask} onChange={handleNewTaskOnChange} />
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            name="addTask"
+            value={newTask}
+            onChange={handleNewTaskOnChange}
+            required
+          />
           <button type="submit">
             Criar
             <PlusCircle size={20} weight="bold" />
@@ -37,17 +54,21 @@ export function Dashboard() {
       <div className={styles.container}>
         <div className={styles.counters}>
           <div className={styles.allTasks}>
-            <p>Tarefas criadas <span>{allTasksCreated}</span></p>
+            <p>
+              Tarefas criadas <span>{allTasksCreated}</span>
+            </p>
           </div>
-          
+
           <div className={styles.finishedTasks}>
-            <p>Concluídas <span>{tasksProgress}</span></p>
+            <p>
+              Concluídas <span>{tasksProgress}</span>
+            </p>
           </div>
         </div>
 
         {tasks.length > 0 ? (
           <div className={styles.withContent}>
-            {tasks.map(task => (
+            {tasks.map((task) => (
               <Task
                 key={task}
                 content={task}
@@ -60,12 +81,12 @@ export function Dashboard() {
           <div className={styles.empty}>
             <img src={emptyClipboard} alt="Dashboard vazio" />
             <p>
-              <strong>Você ainda não tem tarefas cadastradas</strong>          
+              <strong>Você ainda não tem tarefas cadastradas</strong>
             </p>
             <p>Crie tarefas e organize seus itens a fazer</p>
           </div>
         )}
       </div>
     </>
-  )
+  );
 }
--- src/hooks/useTasks/index.tsx
+++ src/hooks/useTasks/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState } from "react";
 
 export const useTask = (value: string) => {
   const [tasks, setTasks] = useState<string[]>([]);
@@ -6,37 +6,39 @@ export const useTask = (value: string) => {
   const [clearField, setClearField] = useState(false);
 
   const onCreateNewTask = (event: FormEvent) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const taskAlreadyCreated = tasks.find(task => task === value);
+    const taskAlreadyCreated = tasks.find((task) => task === value);
     if (taskAlreadyCreated) {
-      alert('Essa tarefa já foi criada!')
+      alert("Essa tarefa já foi criada!");
     } else {
-      setTasks([...tasks, value])
-      setClearField(true)
+      setTasks([...tasks, value]);
+      setClearField(true);
     }
 
     setTimeout(() => {
-      setClearField(false)
-    }, 1000)
-  }
+      setClearField(false);
+    }, 1000);
+  };
 
   const onDeleteTask = (taskToDelete: string) => {
-    const allTasks = tasks.filter(task => task !== taskToDelete);
+    const allTasks = tasks.filter((task) => task !== taskToDelete);
     setTasks(allTasks);
-  }
+  };
 
   const onMarkTask = (taskToMark: string) => {
     const allTasksMarkAsDone = tasksDone;
-    const hasTask = allTasksMarkAsDone.find(task => task === taskToMark);
-    
+    const hasTask = allTasksMarkAsDone.find((task) => task === taskToMark);
+
     if (hasTask) {
-      const removeTaskFromTheList = tasksDone.filter(task => task !== taskToMark);
-      setTasksDone(removeTaskFromTheList)
+      const removeTaskFromTheList = tasksDone.filter(
+        (task) => task !== taskToMark
+      );
+      setTasksDone(removeTaskFromTheList);
     } else {
-      setTasksDone([...tasksDone, taskToMark])
+      setTasksDone([...tasksDone, taskToMark]);
     }
-  }
+  };
 
   return {
     tasks,
@@ -44,6 +46,6 @@ export const useTask = (value: string) => {
     clearField,
     onCreateNewTask,
     onDeleteTask,
-    onMarkTask
-  }
-}
+    onMarkTask,
+  };
+};
